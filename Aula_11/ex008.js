ccb64e3cf28ff1e29709ac97b43481fcabe39a37
Joao <joaopedro767767@gmail.com>
2022-03-16 20:36:26 -0300

    Primeiras condições

---
+++ Aula_11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5;
+console.log(`A velociadade do seu carro é ${vel}km/h`);
+
+if(vel > 60){
+    console.log('Voce ultrapassou a velocidade permitida, MULTADO!');
+}
+
+console.log('Dirija sempre usuando cinto de segurança!!');
---
+++ Aula_11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'EUA';
+console.log(`Morando em ${pais}`);
+
+if(pais == 'Brasil'){
+    console.log('Voce é Brasileira');
+} else{
+    console.log('Voce é Estrangeiro!');
+}

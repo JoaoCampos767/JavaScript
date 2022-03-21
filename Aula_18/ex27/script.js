06d242ba852de7d18b5937ec140b3e72675ebd75
João Pedro Campos <joaopedro767767@gmail.com>
2022-03-20 21:58:36 -0300

    Iniciando a inteligencia do js

--- Aula_18/ex27/script.js
+++ Aula_18/ex27/script.js
@@ -2,13 +2,17 @@ function carregar(){
     var msg = window.document.getElementById('msg');
     var img = window.document.getElementById('imagem');
     var data = new Date();
-    msg.innerHTML = `Agora sao ${hora}`;
+    var hora = data.getHours();
+    msg.innerHTML = `Agora sao ${hora} horas.`;
 
     if(hora >= 0 && hora < 12){
-        img.src = 'manha.jpg'
-    } else if(hora >= 12 && hora < 18){
-        img.src = 'tarde.jpg'
+        img.src = 'manha.png';
+        document.body.style.background = '#fdd7ca';
+    } else if(hora >= 12 && hora <= 18){
+        img.src = 'tarde.png';
+        document.body.style.background = '#65381e';
     } else {
-        img.src = 'noite.jpg'
+        img.src = 'noite.png';
+        document.body.style.background = '#254d7d';
     }
 }

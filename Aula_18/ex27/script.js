41646dd1be857c64b958201293b75b2e23d52f69
João Pedro Campos <joaopedro767767@gmail.com>
2022-03-20 21:33:57 -0300

    Ajuste de pastas

--- Aula_18/ex27/script.js
+++ Aula_18/ex27/script.js
@@ -1,1 +1,14 @@
-alert('Ola');
+function carregar(){
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('imagem');
+    var data = new Date();
+    msg.innerHTML = `Agora sao ${hora}`;
+
+    if(hora >= 0 && hora < 12){
+        img.src = 'manha.jpg'
+    } else if(hora >= 12 && hora < 18){
+        img.src = 'tarde.jpg'
+    } else {
+        img.src = 'noite.jpg'
+    }
+}
